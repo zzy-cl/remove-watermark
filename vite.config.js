384f222eb358b90a1498ab3f80c9b8370cdef45f
赵泽宇ZZY <1580574280@qq.com>
2023-05-12 20:53:30 +0800

    添加工作流

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
+    plugins: [vue()], resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }, build: {
+        outDir: 'dist', assetsDir: 'assets', rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name][extname]'
+            }
         }
     }
 })
